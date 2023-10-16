73e74b4c383c67c9190d0eefa6fdfdf5931fdc8a
Galymzhan11 <93948937+Galymzhan11@users.noreply.github.com>
2023-10-17 03:44:03 +0600

    AstanaIT.sol

--- AstanaIT.sol
+++ AstanaIT.sol
@@ -1,1 +1,78 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.4;
+
+import "@openzeppelin/contracts@4.5.0/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts@4.5.0/token/ERC721/extensions/ERC721Enumerable.sol";
+import "@openzeppelin/contracts@4.5.0/security/Pausable.sol";
+import "@openzeppelin/contracts@4.5.0/access/Ownable.sol";
+import "@openzeppelin/contracts@4.5.0/utils/Counters.sol";
+
+contract AstanaIT is ERC721, ERC721Enumerable, Pausable, Ownable {
+
+
+
+    using Counters for Counters.Counter;
+
+    Counters.Counter private _tokenIdCounter;
+
+    uint256 public MINT_PRICE = 0.05 ether;
+    uint public MAX_SUPPLY = 100;
+
+
+
+    constructor() ERC721("Astana IT", "AITU") {
+        
+        _tokenIdCounter.increment();
+    }
+
+    function withdraw() public onlyOwner() {
+        require(address(this).balance > 0, "Balance is zero");
+        payable(owner()).transfer(address(this).balance);
+    }
+
+
+
+    function pause() public onlyOwner {
+        _pause();
+    }
+
+    function unpause() public onlyOwner {
+        _unpause();
+    }
+
+    function safeMint(address to) public payable {
+        
+        require(totalSupply() < MAX_SUPPLY, "Can't mint anymore tokens.");
+
+        
+        require(msg.value >= MINT_PRICE, "Not enough ether sent.");
+        uint256 tokenId = _tokenIdCounter.current();
+        _tokenIdCounter.increment();
+        _safeMint(to, tokenId);
+    }
+
+
+
+    function _baseURI() internal pure override returns (string memory) {
+        return "https://ipfs.io/ipfs/QmQLLGjSLowYQWc9eEmM4xerRevzjKxofLTj6JrE9tjtHJ";
+    }
+
+    function _beforeTokenTransfer(address from, address to, uint256 tokenId)
+        internal
+        whenNotPaused
+        override(ERC721, ERC721Enumerable)
+    {
+        super._beforeTokenTransfer(from, to, tokenId);
+    }
+
+
+    function supportsInterface(bytes4 interfaceId)
+        public
+        view
+        override(ERC721, ERC721Enumerable)
+        returns (bool)
+    {
+        return super.supportsInterface(interfaceId);
+    }
+}
 
